c837aaed9de685e3321ca01957c5e127ba816d1f
Awerar <alexander.wahlsten@gmail.com>
2021-08-24 10:32:54 +0200

    Started with lexical analysis

--- lexical_analysis.cpp
+++ lexical_analysis.cpp
@@ -1,1 +1,16 @@
 #include "lexical_analysis.h"
+
+vector<string> parse_tokens(string program, grammar_t* grammar)
+{
+	vector<string> tokens;
+
+	while (program.length() > 0) {
+		bool has_parsed = false;
+
+		for (auto& parser : grammar->terminal_parsers) {
+			
+		}
+
+		assert(has_parsed);
+	}
+}
--- lexical_analysis.h
+++ lexical_analysis.h
@@ -1,2 +1,9 @@
 #pragma once
 
+#include <vector>
+#include <string>
+#include "grammar_t.h"
+
+using namespace std;
+
+vector<string> parse_tokens(string program, grammar_t* grammar);
