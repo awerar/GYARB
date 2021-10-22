1f2a95dd4a53a64f64c51f8ab341af58ef3f303f
Awerar <alexander.wahlsten@gmail.com>
2021-10-22 08:54:58 +0200

    Code gen structure work

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main()
 {
     initialize_grammar();
 
-    Lexer* lexer = get_lexer("program.txt");
+    Lexer* lexer = get_lexer("SimpleProgram.txt");
     Parser* parser = new Parser(lexer);
     ParseNode* cst = parser->parse();
     ASTNode* ast = generate_ast(cst);
@@ -28,3 +28,42 @@ int main()
 Lexer* get_lexer(string file_name) {
     return new Lexer(new ifstream(file_name));
 }
+
+//Lambda in memory:
+/*
+int type
+
+0: Var
+    Lambda* var_mem
+
+1: Call
+    Lambda* func
+    Lambda* param
+
+2: Definition
+    Lambda* content
+    Lambda* var
+
+3: Builtin
+    int id
+    
+*/
+
+/*
+%0 = [0, NULL]
+
+%1 = reserve(3);
+%2 = [2, %0, %1]
+%0[1] = %1
+
+%3 = reserve(3)
+%4 = [2, %2, %3]
+
+%5 = reserve(3)
+%6 = [1, %5, %4]
+
+%7 = [2, %6, %5]
+
+%8 = [3, 0]
+%9 = [1, %7, %8]
+*/
